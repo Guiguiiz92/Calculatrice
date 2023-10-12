49db77bb5e055eab09de2365baf0cebb295682fd
Guittonneau <guittonneau92@sfr.fr>
2023-10-12 16:33:30 +0200

    Js liste keys

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-console.log("toto");
+// Liste nombre
+const numberKeys = [...document.querySelectorAll(".line > .number")];
+const listeNumber = numberKeys.map((key) => key.value);
+console.log(listeNumber);
 
-const button = document.querySelector("#one");
-console.log(button);
-button.addEventListener("click", function () {
-  alert("1");
+// Liste opérateur
+const operationKeys = [...document.querySelectorAll(".line > .operation")];
+const listeOperation = operationKeys.map((key) => key.value);
+console.log(listeOperation);
+
+// Liste other
+const otherKeys = [...document.querySelectorAll(".line > .other")];
+const listeOther = otherKeys.map((key) => key.innerHTML);
+console.log(listeOther);
+
+// écouteur d'event
+
+document.addEventListener("keydown", (e) => {
+  const valeur = e.key;
+  console.log(valeur);
+});
+
+document.addEventListener("click", (e) => {
+  const valeur = e.target.dataset.key;
+  console.log(valeur);
 });
