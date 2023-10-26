ee1720a424e3f4d8e42d968a444ab2645bbab83e
Guittonneau <guittonneau92@sfr.fr>
2023-10-26 14:32:36 +0200

    Ajoute calcule et condition

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+//Mon écran de calcul
+const ecranCalcul = document.querySelector("#operation");
+const ecranResultat = document.querySelector("#resultat");
+
 // Liste nombre
 const numberKeys = [...document.querySelectorAll(".line > .number")];
 const listeNumber = numberKeys.map((key) => key.value);
@@ -10,17 +14,87 @@ console.log(listeOperation);
 
 // Liste other
 const otherKeys = [...document.querySelectorAll(".line > .other")];
-const listeOther = otherKeys.map((key) => key.innerHTML);
+const listeOther = otherKeys.map((key) => key.value);
 console.log(listeOther);
 
-// écouteur d'event
+// Liste action
+const actionKeys = [...document.querySelectorAll(".line > .action")];
+const listeAction = actionKeys.map((key) => key.value);
+console.log(listeAction);
+
+// Liste all
+const allowedValues = [
+  ...listeNumber,
+  ...listeOperation,
+  ...listeOther,
+  "(",
+  ")",
+];
+
+const buttonList = [
+  ...numberKeys,
+  ...operationKeys,
+  ...otherKeys,
+  ...actionKeys,
+];
+
+// Function qui va afficher
+function afficher(value) {
+  const lastIndex = ecranCalcul.innerHTML.length - 1;
+  const lastElement = ecranCalcul.innerHTML[lastIndex];
+  if (listeOperation.includes(value)) {
+    if (lastIndex > -1 && replaceOperator(value, lastElement)) {
+      ecranCalcul.innerHTML = ecranCalcul.innerHTML.slice(0, -1) + value;
+    } else if (lastIndex > -1) {
+      ecranCalcul.innerHTML += value;
+    }
+  } else {
+    ecranCalcul.innerHTML += value;
+  }
+}
+function replaceOperator(value, lastElement) {
+  return (
+    listeOperation.includes(lastElement) &&
+    !((lastElement === "/" || lastElement === "*") && value === "-")
+  );
+}
+
+// Function pour afficher/delete/back
+function manageEvent(value) {
+  switch (value) {
+    case "=":
+    case "Enter":
+      const calcul = eval(ecranCalcul.innerHTML);
+      ecranResultat.innerHTML = calcul;
+      break;
+    case "del":
+    case "Backspace":
+      ecranCalcul.innerHTML = ecranCalcul.innerHTML.slice(0, -1);
+      break;
+    case "C":
+    case "Delete":
+      ecranCalcul.innerHTML = "";
+      ecranResultat.innerHTML = ""; // Réinitialise le contenu de l'écran de calcul
+      break;
+    case "()":
+      break;
+    default:
+      if (allowedValues.includes(value)) {
+        afficher(value);
+      }
+  }
+}
+
+// écouteur d'event affichage
 
 document.addEventListener("keydown", (e) => {
-  const valeur = e.key;
-  console.log(valeur);
+  const value = e.key;
+  manageEvent(value);
 });
 
-document.addEventListener("click", (e) => {
-  const valeur = e.target.dataset.key;
-  console.log(valeur);
-});
+for (const button of buttonList) {
+  button.addEventListener("click", (e) => {
+    const value = e.target.value || button.value;
+    manageEvent(value);
+  });
+}
